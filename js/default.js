4f29764c83a700c85d3cf44475349ee729e26110
g1527061 <babon0425@gmail.com>
2016-06-27 17:32:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,23 @@
 
+var include_Fizz-Buzz = function(F-B){
+  if(F-B === 3){
+    message = 'Fizz';
+  }
+  else if(F-B === 5){
+    message = 'Buzz';
+  }
+  else if(F-B === 15){
+    message = 'Fizz Buzz';
+  }
+  else{
+    message = 'F-B';
+  }
+}
+for(var i=1;i<100;i++){
+  var li = document.createElement('li');
+  li.textContent = i + '回目';
+  document.getElementById('list').appendChild(li);
+}
+document.getElementById('form').onsubmit = function(){
+  window.alert(document.getelementById('form').include_Fizz-Buzz(message).value);
+};
